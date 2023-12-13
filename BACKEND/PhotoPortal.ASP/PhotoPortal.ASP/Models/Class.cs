0661a50e6aa6f8746d8bbb7a18676fdc83845d35
Tamás Török <torokt21@gmail.com>
2023-12-13 23:38:01 +0100

    Add class

---
+++ BACKEND/PhotoPortal.ASP/PhotoPortal.ASP/Models/Class.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhotoPortal.ASP.Models
+{
+    public class Class
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        public int InstitutionId { get; set; }
+
+        public Institution Institution { get; set; }
+    }
+}
