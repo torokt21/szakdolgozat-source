cafe98032a6df00b41fff8743dca6dfd6f1096d1
Tamás Török <torokt21@gmail.com>
2024-01-09 20:27:08 +0100

    Delete child folder

--- FRONTEND/src/components/pages/admin/photo/upload/UploadFileBrowser.tsx
+++ FRONTEND/src/components/pages/admin/photo/upload/UploadFileBrowser.tsx
@@ -1,10 +1,10 @@
+import { Box, Grid, Tooltip } from "@mui/material";
 import { TreeItem, TreeView } from "@mui/x-tree-view";
 
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { FileWithPath } from "react-dropzone";
-import { Grid } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
@@ -23,7 +23,7 @@ type UploadFileBrowserProps = {
 };
 
 export default function UploadFileBrowser(props: UploadFileBrowserProps) {
-	function handleDeleteClick(child: UploadChild, file: FileWithPath) {
+	function handlePictureDeleteClick(child: UploadChild, file: FileWithPath) {
 		const clone = _.cloneDeep(props.uploadInstitution);
 
 		const foundChild = clone.classes
@@ -41,6 +41,20 @@ export default function UploadFileBrowser(props: UploadFileBrowserProps) {
 
 		// Delete empty class folder
 		clone.classes = clone.classes.filter((c) => c.children.length > 0);
+
+		props.onFilesChanged(clone);
+	}
+
+	function handleChildDelete(child?: UploadChild) {
+		if (!child) return;
+		if (!confirm("Biztosan törölni szeretnéd az egész mappát?")) return;
+
+		const clone = _.cloneDeep(props.uploadInstitution);
+
+		// Delete empty child folder
+		clone.classes.forEach((cl) => {
+			cl.children = cl.children.filter((c) => c.fullPath !== child.fullPath);
+		});
 
 		props.onFilesChanged(clone);
 	}
@@ -70,7 +84,19 @@ export default function UploadFileBrowser(props: UploadFileBrowserProps) {
 					<ImageList cols={4}>
 						<ImageListItem key="Subheader" cols={4}>
 							<ListSubheader component="div">
-								Képek a következő mappából: {props.selectedChild?.fullPath}
+								<Box sx={{ display: "flex" }} pb={3}>
+									<Box sx={{ flexGrow: 1 }}>
+										<strong>Képek a következő mappából:</strong>{" "}
+										{props.selectedChild?.fullPath}
+									</Box>
+									<Tooltip title="Mappa törlése">
+										<IconButton
+											color="error"
+											onClick={() => handleChildDelete(props.selectedChild)}>
+											<DeleteIcon />
+										</IconButton>
+									</Tooltip>
+								</Box>
 							</ListSubheader>
 						</ImageListItem>
 						{props.selectedChild?.pictures.map((item) => (
@@ -88,7 +114,10 @@ export default function UploadFileBrowser(props: UploadFileBrowserProps) {
 										<IconButton
 											sx={{ color: "rgba(255, 255, 255, 0.54)" }}
 											onClick={() => {
-												handleDeleteClick(props.selectedChild!, item);
+												handlePictureDeleteClick(
+													props.selectedChild!,
+													item
+												);
 											}}>
 											<DeleteIcon />
 										</IconButton>
